d9c26f78e1de14c2885aff9c98d068175338c2a9
Yu Nakamura <89377497+yoomdrl@users.noreply.github.com>
2021-09-16 06:09:33 +0900

    Initial commit

---
+++ Assets/Scripts/GameCountdown.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameCountdown : MonoBehaviour
+{
+    public float countdown = 4.0f;
+    public GameObject WinText;
+    public GameObject LoseText;
+    public GameObject Rock;
+    public GameObject Paper;
+    public GameObject Scissors;
+    public Text CountDownText;
+    public int random = Random.Range(0, 3);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      CountDownText.text = countdown.ToString();
+      if(countdown < 0)
+      {
+        countdown -= Time.deltaTime;
+        if(countdown <= 3)
+        {
+          WinText.SetActive(true);
+        }
+        if(countdown <= 2)
+        {
+          if(random == 0)
+          {
+            Rock.SetActive(true);
+          }
+          if(random == 1)
+          {
+            Paper.SetActive(true);
+          }
+          if(random == 2)
+          {
+            Scissors.SetActive(true);
+          }
+        }
+        if(countdown >= 0 && countdown <= 1)
+        {
+
+        }
+      }
+
+    }
+}
