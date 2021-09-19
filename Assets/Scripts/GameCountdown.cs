e252d5d7f16ee1629bc89416a443331410dcabdd
Yu Nakamura <89377497+yoomdrl@users.noreply.github.com>
2021-09-19 23:36:04 +0900

    なぜか三問目に行かないです

--- Assets/Scripts/GameCountdown.cs
+++ Assets/Scripts/GameCountdown.cs
@@ -11,11 +11,14 @@ public class GameCountdown : MonoBehaviour
     public GameObject Rock;
     public GameObject Paper;
     public GameObject Scissors;
-    public int random;
     public GameObject ButtonRock;
     public GameObject ButtonPaper;
     public GameObject ButtonScissors;
-    public GameObject ScoreText;
+    public Text ScoreText;
+    public int random;
+    public int score = 0;
+
+
 
     public int answered = 0;
 
@@ -23,27 +26,29 @@ public class GameCountdown : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-//      random = Random.Range(0, 3);
-      random = 0;
+      random = Random.Range(0, 3);
       ButtonRock = GameObject.Find("ButtonRock");
       ButtonPaper = GameObject.Find("ButtonPaper");
       ButtonScissors = GameObject.Find("ButtonScissors");
-      ScoreText = GameObject.Find("ScoreText");
     }
 
     // Update is called once per frame
     void Update()
     {
+      ScoreText.text = score.ToString();
       //タイマーが0になるまで問題ワンセット
       if(countdown > 0.0)
       {
+        // if(countdown == 3.0)
+        // {
+        //   answered = 0;
+        // }
         countdown -= Time.deltaTime;
 
         //勝敗の指定の表示
         if(countdown <= 2.0)
         {
           WinText.SetActive(true);
-          answered = 0;
         }
         //相手の手がグーチョキパーの三択から出る
         if(countdown <= 1.0)
@@ -71,36 +76,80 @@ public class GameCountdown : MonoBehaviour
             if(ButtonRock.GetComponent<RockPressed>().rockSelect == true)
             {
               answered++;
-              Debug.Log("Incorrect");
+              Debug.Log("GameOver");
             }
             if(ButtonPaper.GetComponent<PaperPressed>().paperSelect == true)
             {
               answered++;
-              ScoreText.GetComponent<ScoreScript>().addScore();
+              score++;
               Debug.Log("Correct");
             }
             if(ButtonScissors.GetComponent<ScissorsPressed>().scissorsSelect == true)
             {
               answered++;
-              Debug.Log("Incorrect");
+              Debug.Log("GameOver");
+            }
+          }
+
+          //パーだった時
+        if(random == 1 && answered == 0)
+          {
+            if(ButtonRock.GetComponent<RockPressed>().rockSelect == true)
+            {
+              answered++;
+              Debug.Log("GameOver");
+            }
+            if(ButtonPaper.GetComponent<PaperPressed>().paperSelect == true)
+            {
+              answered++;
+              Debug.Log("GameOver");
+            }
+            if(ButtonScissors.GetComponent<ScissorsPressed>().scissorsSelect == true)
+            {
+              answered++;
+              score++;
+              Debug.Log("Correct");
+            }
+          }
+
+            //チョキだった時
+        if(random == 2 && answered == 0)
+          {
+            if(ButtonRock.GetComponent<RockPressed>().rockSelect == true)
+            {
+              answered++;
+              score++;
+              Debug.Log("Correct");
+            }
+            if(ButtonPaper.GetComponent<PaperPressed>().paperSelect == true)
+            {
+              answered++;
+              Debug.Log("GameOver");
+            }
+            if(ButtonScissors.GetComponent<ScissorsPressed>().scissorsSelect == true)
+            {
+              answered++;
+              Debug.Log("GameOver");
             }
           }
         }
-        //未回答だった場合負け
-        if(countdown < 0 && answered == 0)
-        {
-          Debug.Log("GameOver");
-        }
       }
-          // if(random == 1)
-          // {
-          //
-          // }
-          // if(random == 2)
-          // {
-          //
-          // }
-
+      //未回答の場合負け
+      if(countdown < 0.0 && answered == 0)
+      {
+        Debug.Log("GameOver");
+      }
+      //正解を出せた場合、次の問題に移行　要するにリセット
+      if(countdown < 0.0 && answered == 1)
+      {
+        countdown = 3.0f;
+        answered = 0;
+        Rock.SetActive(false);
+        Paper.SetActive(false);
+        Scissors.SetActive(false);
+        WinText.SetActive(false);
+        random = Random.Range(0, 3);
+      }
     }
 
 }
